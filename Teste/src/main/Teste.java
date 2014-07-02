c3c0b2bb32706639af0a95922cf8f8c93d339441
Wellington <tomwel6@hotmail.com>
2014-07-02 09:39:03 -0300

    Testando integração com basecamp

--- Teste/src/main/Teste.java
+++ Teste/src/main/Teste.java
@@ -3,6 +3,7 @@ package main;
 public class Teste {
 	
 	String teste = "Testando o merge com gitHub";
-	String teste = "Testando o commite com gitHub";
+	String teste2 = "Testando o commite com gitHub";
+	String teste3 = "Testando integração com basecamp";
 
 }
