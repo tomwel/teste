6489d35be1eba0dfe29d43b801bd8d8b22b99c82
Wellington <tomwel6@hotmail.com>
2014-07-07 14:48:46 -0300

    Update Teste.java
    Pronto

--- Teste/src/main/Teste.java
+++ Teste/src/main/Teste.java
@@ -2,7 +2,7 @@ package main;
 
 public class Teste {
 	
-	String teste = "Testando o merge com gitHub";
+	String teste1 = "Testando o merge com gitHub";
 	String teste2 = "Testando o commite com gitHub";
 	String teste3 = "Testando integração com basecamp";
 
